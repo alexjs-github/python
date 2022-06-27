040286897450be79104d1c590a0c54b49e9630f7
Alexander <alexaparicio.web2@gmail.com>
2022-06-26 23:25:57 -0500

    segundo commit

--- consumoApiR.py
+++ consumoApiR.py
@@ -1,7 +1,20 @@
+from flask import Flask
 import requests
 import json
 
 print('Cargando datos...')
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def home():
+  return 'HOLA ALEXANDER'
+
+app.route("/about")
+
+def about():
+  return 'ABOUT'
 
 def peticion (url):
 
@@ -18,3 +31,6 @@ URL='http://pokeapi.co/api/v2/pokemon/'
 
 for pokemon in peticion(URL)["results"]:
   print(pokemon)
+
+if __name__ == '__main__':
+  app.run()
